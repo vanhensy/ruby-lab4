514b519c7ac5b471de513125aafd0b4b9ff07bb1
Harley Trung <harley@bsa.edu.vn>
2016-03-29 15:33:08 +0700

    add li_link_to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def li_link_to(name, url, options = {})
+    options[:class] ||= ""
+    options[:class] << " nav-link"
+    content_tag(:li, class: 'nav-item') do
+      link_to(name, url, options)
+    end
+  end
 end
