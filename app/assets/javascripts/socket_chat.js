6d79565f5e73510a34844073a011def5ba9843f6
vanhensy <anhvynv.vanhensy@gmail.com>
2016-04-01 02:22:05 +0700

    Update new UI

--- app/assets/javascripts/socket_chat.js
+++ app/assets/javascripts/socket_chat.js
@@ -2,20 +2,22 @@
   // please type it yourself, do NOT copy & paste
   window.ws = new WebSocket("ws://" + window.document.location.host)
   ws.onmessage = function(message) {
-  	console.log(message.data)
+  	// console.log(message.data)
   	var data = JSON.parse(message.data)
   	var messageHtml = data.content
-  	$(".messages").append("<p>" + messageHtml + "</p>")
-    console.log("received: ", messageHtml)
+    var contentMesssage = '<div class=\"talk-bubble tri-right left-top slideInUp\"><div class=\"talktext\"><p>' + messageHtml + '</p></div></div> <br>';
+  	$(".messages").append(contentMesssage)
+    // console.log("received: ", contentMesssage)
+    $('.message-box').animate({scrollTop: $('.message-box').get(0).scrollHeight}, 1000);
   };
 
   function setupForm() {
     $("form#new_message").on("submit", function(event) {
-      event.preventDefault();
       var content = $("#message_content").val();
       var user    = "fix later";
       ws.send(JSON.stringify({ content: content, user: user }));
-      $("#message_content").val('')
+      $("#message_content").val(null)
+
   }) };
 
   $(document).on("page:change", function(){
